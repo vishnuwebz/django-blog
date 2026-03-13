b75c359da2c33dc002db068e8418989aed222b63
Vishnu G <vishnugkannath@gmail.com>
2026-03-13 22:44:13 +0530

    Module 3: Add views, dynamic URL patterns, and redirects

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,19 @@ from . import views
 app_name = "blog"
 
 urlpatterns = [
+    # home page
     path("", views.index, name="index"),
+
+    # detail page using integer ID
+    path("post/<int:post_id>/", views.post_detail, name="post-detail"),
+
+    # detail page using slug (we'll use this more seriously later)
+    path("post/slug/"
+         "<slug:slug>/", views.post_detail_slug, name="post-detail-slug"),
+
+    # new url
+    path("new-url/", views.new_blog_url, name="new-blog-url"),
+
+    # old url that should redirect to the new url
+    path("old-url/", views.old_blog_url, name="old-blog-url"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,37 @@
 from django.http import HttpResponse
+from django.shortcuts import redirect
+from django.urls import reverse
 
 def index(request):
-    return HttpResponse("Hello, world.")
+    return HttpResponse("Hello, world. this is the blog home page!")
+
+def post_detail(request, post_id: int):
+    """
+    Temporary detail view using an integer ID.
+    In a future module, we'll fetch data from the database using this ID.
+    """
+    return HttpResponse(f"Post detail page for post ID: {post_id}")
+
+def post_detail_slug(request, slug: str):
+    """
+    Temporary detail view using a slug.
+    Later, we'll use this to fetch posts by slug from the database.
+    :param request:
+    :param slug:
+    :return:
+    """
+    return HttpResponse(f"Post detail page for slug: {slug}")
+
+def new_blog_url(request):
+    """
+    This represents a new URL users should use.
+    """
+    return HttpResponse("You have been redirected to the NEW blog URL.")
+
+def old_blog_url(request):
+    """
+    This view demonstrates how to redirect from an old URL to a new one.
+    We use the URL name instead of hard-coding the path.
+    """
+    new_url = reverse("blog:new-blog-url")
+    return redirect(new_url)
